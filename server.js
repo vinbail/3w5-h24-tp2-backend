087044ae0537ba23bf0b7dd297815b284b91a101
Vincent Baillargeon <vinbail@dinfo.cegepthetford.ca>
2024-02-21 15:22:31 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const classes = ["warrior", "mage", "ranger"];
+
+app.get("/newClass", (req, res) => {
+  const index = Math.floor(Math.random() * Math.floor(3));
+  console.log(index);
+  const yourNewClass = classes[index];
+  res.send(yourNewClass);
+});
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT);
+console.log("backend started");
